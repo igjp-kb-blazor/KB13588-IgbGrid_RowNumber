8867c1d68107fe9442d94fe008ea6e44aab37983
mkamiishi <mkamiishi@infragistics.com>
2024-05-07 17:23:43 +0900

    Initial commit
    初回コミット

---
+++ IgbGrid_RowNumber/wwwroot/JavaScript.js
@@ -0,0 +1,8 @@
+﻿igRegisterScript("WebGridRowSelectorExcelTemplate", (ctx) => {
+    var html = window.igTemplating.html;
+    return html`<span style='display: block;width:50px; text-align: center;'> ${ctx.implicit.index}</span>`;
+}, false);
+igRegisterScript("WebGridHeaderRowSelectorExcelTemplate", (ctx) => {
+    var html = window.igTemplating.html;
+    return html`<span style='display: block;width:50px;'></span>`;
+}, false);
